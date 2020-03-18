57d8a24bad75bd7b54d4a47e34741927dd3db914
uyouthe <melvoloskov@ya.ru>
2020-03-18 11:23:05 +0300

    basic signup

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const app = express()
 
 mongoose.connect(process.env.MONGODB_URI, {
@@ -9,11 +10,25 @@ mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
 })
 
+const User = mongoose.model('User', { email: String, password: String })
+
 app.use(express.static(path.join(__dirname, '/../build')))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/api/getList', (req, res) => {
   var list = ['item1', 'item2', 'item3', 'item4']
   res.json(list)
+})
+
+app.post('/api/signup', (req, res) => {
+  const newUser = new User({
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .save()
+    .then(() => {
+      res.json({ ok: 'true' })
+    })
 })
 
 app.get('*', (req, res) => {
