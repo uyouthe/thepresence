4daa3da0f201b6399956ca78c73a368687e261d6
uyouthe <melvoloskov@ya.ru>
2020-03-18 11:13:41 +0300

    mongoose added

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const path = require('path')
-
+const mongoose = require('mongoose')
 const app = express()
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 app.use(express.static(path.join(__dirname, '/../build')))
 
