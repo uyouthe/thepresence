b20da5a8214673208d97dbc14c5fee774a517b63
uyouthe <melvoloskov@ya.ru>
2020-03-18 11:05:56 +0300

    dotenv added with mongo url

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
 
@@ -18,3 +19,4 @@ const port = process.env.PORT || 5000
 app.listen(port)
 
 console.log('App is listening on port ' + port)
+// console.log(process.env.MONGODB_URI)
